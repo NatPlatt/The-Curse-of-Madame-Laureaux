d73396d15b72b1d0d7eb0779de04a4ca2ab7e761
NatPlatt <n.zollinger26@hotmail.com>
2020-02-02 17:44:58 -0700

    writing charController script

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,4 +8,22 @@ public class CharController : MonoBehaviour
     private CharacterController charController;
 
     public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    
+    private Vector3 moveDirection = Vector3.zero;
+
+    private void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (charController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+        }
+    }
 }
