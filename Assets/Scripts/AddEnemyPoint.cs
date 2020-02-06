744ec72a37da1dc5b8abd9e98c2db16fecb7d2a4
NatPlatt <n.zollinger26@hotmail.com>
2020-02-06 16:19:30 -0700

    writing script to add enemy patrol points

---
+++ Assets/Scripts/AddEnemyPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddEnemyPoint : MonoBehaviour
+{
+    public static Action<Transform> SendTransformAsPoint;
+    void Start()
+    {
+        if (SendTransformAsPoint != null)
+        {
+            SendTransformAsPoint(transform);
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
