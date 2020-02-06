fd2e31ac80b71829426bad32bb535af6a9830bb5
NatPlatt <n.zollinger26@hotmail.com>
2020-02-06 16:29:35 -0700

    writing patrol movement script

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed = 1f;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    
+    void Update()
+    {
+        transform.position =
+            Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+    }
+}
