52a68ce02ab297858421438f00bdd5fb3e7936a7
NatPlatt <n.zollinger26@hotmail.com>
2020-02-07 15:07:45 -0700

    writing scripts so player can interact with objects
    hidden clues, and enemys

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -7,6 +7,43 @@ public class Interactable : MonoBehaviour
 {
     public float radius = 3f;
 
+    private bool isFocus;
+    private Transform player;
+
+    private bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        //this method is meant to change depending on who it's used on
+        Debug.Log("interacting with" + transform.name);
+    }
+    private void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance <= radius)
+            {
+                Debug.Log("I am a hidden clue");
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+    
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+    
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
