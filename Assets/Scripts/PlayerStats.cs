15dd14631eb27e21276b7b2c47e506b49fcd8c56
NatPlatt <n.zollinger26@hotmail.com>
2020-02-07 13:19:52 -0700

    made scriptable object and script to control it

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerStats : ScriptableObject
+{
+    public float health;
+    public float XP;
+    
+}
