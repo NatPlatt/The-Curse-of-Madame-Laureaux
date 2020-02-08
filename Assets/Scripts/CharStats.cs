b4f81367107b217aa9cdf393908338cad595751b
NatPlatt <n.zollinger26@hotmail.com>
2020-02-08 12:33:00 -0700

    writing scripts for stats in game

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    public int maxHP = 100;
+    public int currentHP { get; private set; }
+    
+    public Stats damage;
+
+    private void Awake()
+    {
+        currentHP = maxHP;
+    }
+}
