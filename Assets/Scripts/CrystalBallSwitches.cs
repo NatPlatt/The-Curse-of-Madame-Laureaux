4ca081a0802f5de17774e010fa1b0d2020e6f4df
NatPlatt <n.zollinger26@hotmail.com>
2020-02-12 15:02:52 -0700

    writing scene change script

--- Assets/Scripts/CrystalBallSwitches.cs
+++ Assets/Scripts/CrystalBallSwitches.cs
@@ -9,8 +9,18 @@ public class CrystalBallSwitches : MonoBehaviour
     public object crystalBall2;
     public object crystalBall3;
 
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = gameObject.GetComponent<Animator>();
+    }
+
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //myAnimator.SetTrigger("clicked") <-- will later animate when clicked, while also triggering other anims
+        }
     }
 }
