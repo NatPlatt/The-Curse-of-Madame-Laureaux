68a67c4c94142990fa52c4305134befdb0895cde
NatPlatt <n.zollinger26@hotmail.com>
2020-02-12 14:46:49 -0700

    writing script for crystal ball switches

---
+++ Assets/Scripts/CrystalBallSwitches.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalBallSwitches : MonoBehaviour
+{
+    public object crystalBall1;
+    public object crystalBall2;
+    public object crystalBall3;
+
+    private void Update()
+    {
+        
+    }
+}
