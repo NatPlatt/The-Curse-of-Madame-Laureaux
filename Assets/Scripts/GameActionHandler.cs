faf5b7a2c5f6ff627575d574f705b7aeb5cef85d
NatPlatt <n.zollinger26@hotmail.com>
2020-02-13 16:12:09 -0700

    started writing gameActionHandler script

---
+++ Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction gameAction;
+    void Start()
+    {
+        game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
