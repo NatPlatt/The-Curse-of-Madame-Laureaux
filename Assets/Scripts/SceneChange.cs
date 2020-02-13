fd70ee33fec3fa98c787a75573962c048b3116eb
NatPlatt <n.zollinger26@hotmail.com>
2020-02-13 16:06:12 -0700

    added script for scene change

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 [CreateAssetMenu]
 public class SceneChange : ScriptableObject
 {
+    private Object scene;
     public void LoadScene(Object sceneName)
     {
         var newName = sceneName.name;
         SceneManager.LoadScene(newName);
+    }
+
+    public void OnTriggerEnter()
+    {
+        LoadScene(scene);
     }
 }
