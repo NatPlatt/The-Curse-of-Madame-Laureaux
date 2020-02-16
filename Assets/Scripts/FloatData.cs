eaf35a447a0b1a7bec690c1e660893fbf597255a
NatPlatt <n.zollinger26@hotmail.com>
2020-02-16 11:56:07 -0700

    wrote float data script

---
+++ Assets/Scripts/FloatData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+    public float maxValue = 1f;
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+
+    private void UpdateValueLimitZero(float amount)
+    {
+        if (value > 0)
+        {
+            UpdateValue(amount);
+        }
+        else
+        {
+            value = 0;
+        }
+    }
+
+    public void UpdateValueLimitZeroAndMaxValue(float amount)
+    {
+        if (value <= maxValue)
+        {
+            UpdateValue(amount);
+        }
+        else
+        {
+            value = maxValue;
+        }
+        UpdateValueLimitZero(amount);
+    }
+
+    public void ChangeAmount(float amount)
+    {
+        value = amount;
+    }
+}
---
+++ Assets/Scripts/ImageController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageController : MonoBehaviour
+{
+    private Image image;
+    public Stats statObj;
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+    
+    void UpdateImageComponent()
+    {
+        image.fillAmount = statObj.GetValue();
+    }
+}
