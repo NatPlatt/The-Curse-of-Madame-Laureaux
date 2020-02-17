aba37682776d968ab0ae6f82dac8748639500d3e
NatPlatt <n.zollinger26@hotmail.com>
2020-02-17 14:27:38 -0700

    writing particle emission script

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem particles;
+    void Start()
+    {
+        particles.emission.rateOverTime = 0;
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            par
+        }
+    }
+
+   
+}
