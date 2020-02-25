0eb734de04a1ccbf1f72ffb0adf7cc57ddba0e57
NatPlatt <n.zollinger26@hotmail.com>
2020-02-24 18:09:01 -0700

    writing float data math script

---
+++ Assets/Scripts/MatchingSystem/FloatMathSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Systems/Float Math")]
+public class FloatMathSystem : WorkSystem
+{
+    public FloatData dataObj;
+    private FloatData fromNameId;
+    
+    public override void Work()
+    {
+        fromNameId = NameIdObj as FloatData;
+        workEvent.Invoke();
+    }
+}
