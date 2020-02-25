c76683cc8c0257634ce21a8fa78f012267e112c8
NatPlatt <n.zollinger26@hotmail.com>
2020-02-24 19:08:40 -0700

    Update FloatMathSystem.cs

--- Assets/Scripts/MatchingSystem/FloatMathSystem.cs
+++ Assets/Scripts/MatchingSystem/FloatMathSystem.cs
@@ -11,12 +11,22 @@ public class FloatMathSystem : WorkSystem
     
     public override void Work()
     {
-        fromNameId = NameIdObj as FloatData;
+       // fromNameId = NameIdObj as FloatData;   <--problem with this
         workEvent.Invoke();
     }
 
     public void UpdateValue()
     {
         dataObj.UpdateValue(fromNameId.value);
+    }
+
+    public void ChangeValue()
+    {
+        dataObj.value = fromNameId.value;
+    }
+
+    public void IncrementValue()
+    {
+        //dataObj.IncrementValue();   <--problem with this
     }
 }
