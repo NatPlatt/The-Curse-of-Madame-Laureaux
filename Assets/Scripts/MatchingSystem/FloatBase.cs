d2f89a416b3212b664a50eb6a38f78d70917707d
NatPlatt <n.zollinger26@hotmail.com>
2020-02-26 21:09:43 -0700

    writing float base script

---
+++ Assets/Scripts/MatchingSystem/FloatBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "Single Variables/Floatbase")]
+public class FloatBase : ScriptableObject
+{
+    public float value;
+}
