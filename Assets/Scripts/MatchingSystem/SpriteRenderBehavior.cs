3cad449e5e2db84ffcca5518e324444bd1f16167
NatPlatt <n.zollinger26@hotmail.com>
2020-02-26 20:59:09 -0700

    Update SpriteRenderBehavior.cs

--- Assets/Scripts/MatchingSystem/SpriteRenderBehavior.cs
+++ Assets/Scripts/MatchingSystem/SpriteRenderBehavior.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteRenderBehavior : MonoBehaviour
 {
-   
+    private SpriteRenderer spriteRendererObj;
+    public UnityEvent startEvent;
     void Start()
     {
         
