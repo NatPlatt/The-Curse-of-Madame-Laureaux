aa680320398afe845d167077db490c0d5d4ce590
NatPlatt <n.zollinger26@hotmail.com>
2020-02-27 18:17:23 -0700

    writing coin collection script

--- Assets/Scripts/CoinCollection.cs
+++ Assets/Scripts/CoinCollection.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinCollection : MonoBehaviour
+[CreateAssetMenu]
+public class CoinCollection : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
