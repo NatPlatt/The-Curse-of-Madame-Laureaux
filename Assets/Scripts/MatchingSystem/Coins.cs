efbbf9ff9ebf5731b7684e16d9e523b986aac21b
NatPlatt <n.zollinger26@hotmail.com>
2020-02-27 18:15:53 -0700

    Update Coins.cs

--- Assets/Scripts/MatchingSystem/Coins.cs
+++ Assets/Scripts/MatchingSystem/Coins.cs
@@ -6,5 +6,12 @@ using UnityEngine.Events;
 [CreateAssetMenu]
 public class Coins : ScriptableObject
 {
-    
+    public UnityAction<int> coinAction;
+
+    public int value = 10;
+
+    public void Spend()
+    {
+        coinAction?.Invoke(value);
+    }
 }
