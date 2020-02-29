28e5d8b35c19e1a2600573edff6f3a81007090f4
NatPlatt <n.zollinger26@hotmail.com>
2020-02-28 18:02:17 -0700

    Update ToggleEvent.cs

--- Assets/Scripts/ToggleEvent.cs
+++ Assets/Scripts/ToggleEvent.cs
@@ -22,8 +22,19 @@ public class ToggleEvent : ScriptableObject
         raise = TurnOn;
     }
 
-    public void TurnOn()
+    public void OnRaise()
     {
-        
+        raise();
+    }
+
+    private void TurnOn()
+    {
+        onEvent.Invoke();
+        raise = TurnOff;
+    }
+
+    private void TurnOff()
+    {
+        offEvent.Invoke();
     }
 }
