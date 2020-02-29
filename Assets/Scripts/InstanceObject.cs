f6886fe8882e05a1996ba44421b25a208cf5cb45
NatPlatt <n.zollinger26@hotmail.com>
2020-02-29 12:47:46 -0700

    Update InstanceObject.cs

--- Assets/Scripts/InstanceObject.cs
+++ Assets/Scripts/InstanceObject.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Instancing/Instance Object")]
 public class InstanceObject : ScriptableObject
 {
+    public void CreateInstance(GameObject instance)
+    {
+        Instantiate(instance);
+    }
 }
 
