0c0a6b3b5bda7ef8aec83230ed511fc64d772708
NatPlatt <n.zollinger26@hotmail.com>
2020-02-28 17:57:19 -0700

    writing toggle event script

---
+++ Assets/Scripts/ToggleEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class ToggleEvent : ScriptableObject
+{
+    public UnityEvent onEvent;
+    public UnityEvent offEvent;
+    public UnityAction raise;
+    
+    
+}
