83b22a8490cb50ec1b89c7b110d6470754295e9a
NatPlatt <n.zollinger26@hotmail.com>
2020-03-03 12:38:43 -0700

    Update TextBehavior.cs

--- Assets/Scripts/UI/TextBehavior.cs
+++ Assets/Scripts/UI/TextBehavior.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Text))]
 public class TextBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
