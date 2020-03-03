38e79d3abe5fc7d3a58ef8ebb52311f1c6dba98e
NatPlatt <n.zollinger26@hotmail.com>
2020-03-03 14:23:19 -0700

    fixing scripts

--- Assets/Scripts/MatchingSystem/MatchID.cs
+++ Assets/Scripts/MatchingSystem/MatchID.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -7,18 +8,21 @@ using UnityEngine.Serialization;
 public class MatchID : MonoBehaviour
 {
     
-    public NameId id;
-    public UnityEvent onMatch;
-    public UnityEvent noMatch;
-    public bool MatchMade { private get; set; }
-
     [Serializable]
     public struct possibleWork
     {
         public NameId nameIdObj;
         public UnityEvent enterEvent, exitEvent;
     }
-
+    
+    public NameId id;
+    public UnityEvent onMatch;
+    public UnityEvent noMatch;
+    private IdBehavior otherIdBehaviorObj;
+    private NameId otherIdObj;
+    public List<possibleWork> workIdList;
+    public bool MatchMade { private get; set; }
+    
     private void OnTriggerEnter(Collider other)
     {
         var otherId = other.GetComponent<MatchID>();
