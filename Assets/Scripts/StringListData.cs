06fc311b7df677e50658585d83e7b9bd1b99f424
NatPlatt <n.zollinger26@hotmail.com>
2020-03-03 12:51:33 -0700

    writing string list data script

---
+++ Assets/Scripts/StringListData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StringListData : ScriptableObject
+{
+   public List<string> stringListObj;
+
+   public int currentLineNumber;
+
+   public string returnCurrentLine()
+   {
+      return stringListObj[currentLineNumber];
+   }
+}
