df0f23f98771225a0934217cb3ba9fc788c54e91
NatPlatt <n.zollinger26@hotmail.com>
2020-03-04 11:24:46 -0700

    Update SwitchPlayerControl.cs

--- Assets/Scripts/SwitchPlayerControl.cs
+++ Assets/Scripts/SwitchPlayerControl.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchPlayerControl : MonoBehaviour
 {
     public GameObject avatar1, avatar2;
-    
-    
+
+    private int activeAvatar = 1;
+
+    private void Start()
+    {
+        
+    }
 }
