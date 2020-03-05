600ddcfab4232647d6ee5905ee4d51c94985379c
NatPlatt <n.zollinger26@hotmail.com>
2020-03-05 12:33:48 -0700

    writing int data script

---
+++ Assets/Scripts/IntData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Single Variables/IntData")]
+public class IntData : NameId
+{
+   public int value;
+
+   public void SetValue()
+   {
+      
+   }
+}
--- Assets/Scripts/UI/TextBehavior.cs
+++ Assets/Scripts/UI/TextBehavior.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Text))]
-public class TextBehavior : MonoBehaviour
+public class TextBehavior : MonoBehaviour //this is controlled by a gameAction object
 {
     private Text textObj;
     //public StringListData stringListDataObj;
-    
+    public UnityEvent awakeEvent;
+
     void Awake()
     {
         textObj = GetComponent<Text>();
+        awakeEvent.Invoke();
     }
     
     void UpdateStringData(StringListData stringListDataObj)
     {
         textObj.text = stringListDataObj.ReturnCurrentLine();
+    }
+
+    public void UpdateText(IntData intDataObj)
+    {
+        
     }
 }
