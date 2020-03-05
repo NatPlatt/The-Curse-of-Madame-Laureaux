18fa15af526a91138fb624d38482cdd3a729c947
NatPlatt <n.zollinger26@hotmail.com>
2020-03-04 20:20:50 -0700

    trying to instance coins

--- Assets/Scripts/InstanceObject.cs
+++ Assets/Scripts/InstanceObject.cs
@@ -10,21 +10,23 @@ public class InstanceObject : ScriptableObject
     public Vector3 instanceAtLocation;
     public GameAction gameActionObj;
     public GameObject instanceObj;
+    public Transform instanceHere;
     
     public void CreateInstance(GameObject instance)
     {
         for (int i = 0; i < instanceAmount; i++)
         {
-           InstanceSomething(instanceObj);
+           InstanceSomething(instanceHere);
+           instanceAtLocation = Vector3.one;
         }
     }
 
-    public void InstanceSomething(GameObject obj)
+    public void InstanceSomething(Transform location)
     {
         //var newObj = Instantiate(obj, instanceAtLocation, Quaternion.identity);
         //gameActionObj.transformAction(newObj.transform);
 
-        Instantiate(instanceObj, instanceAtLocation, Quaternion.identity);
+        Instantiate(instanceObj, instanceAtLocation, location.rotation);
     }
 }
 
