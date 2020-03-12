c0bd2fad7e7172d020779dea7aca54e3fd4bd8a8
NatPlatt <n.zollinger26@hotmail.com>
2020-03-12 12:37:05 -0600

    fixing bug

---
+++ Assets/Scripts/CharBehavior.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharBehavior : MonoBehaviour
+{
+    protected CharacterController charController;
+    private Vector3 position;
+    private Vector3 movement;
+
+    public float speed = 6.0f;
+    public float rotateSpeed = 2.0f;
+    public float gravity = 20.0f;
+
+    private bool goHorizontal = false;
+    
+    protected Vector3 moveDirection = Vector3.zero;
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+    
+}
