ea012be372476e813648abb9a04f1512aa307ea8
NatPlatt <n.zollinger26@hotmail.com>
2020-03-12 11:49:28 -0600

    taking out unused directives from scripts

--- Assets/Scripts/InstanceObject.cs
+++ Assets/Scripts/InstanceObject.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
+﻿
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Instancing/Instance Object")]
