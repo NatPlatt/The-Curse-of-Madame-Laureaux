27a70603dfb8f6d818e11907cf64bed30f6c76db
NatPlatt <n.zollinger26@hotmail.com>
2020-03-12 16:00:52 -0600

    saved first scene changes to character controller

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -6,53 +8,52 @@ public class CharController : CharBehavior
 {
     public Vector3 rotateDirection;
     private float horizontalInput, verticalInput;
+    Vector3 move = new Vector3(0,0,0);
+    
 
     private void MoveChar()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
         if (charController.isGrounded)
         {
-            if (horizontalInput != 0 && verticalInput != 0)
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            
+            if (horizontalInput != 0 || verticalInput != 0)
             {
-                moveDirection.x = horizontalInput * speed;
-                moveDirection.z = verticalInput * speed;
+                transform.rotation = Quaternion.LookRotation(move);
             }
+            moveDirection.x = Input.GetAxis("Horizontal");
+            moveDirection.z = Input.GetAxis("Vertical");
 
-            if (moveDirection.x != 0 || moveDirection.z != 0)
-            {
-                charController.transform.forward = new Vector3(moveDirection.x, 0, moveDirection.z);
-            }
+
         }
         moveDirection.y -= gravity * Time.deltaTime;
 
-        charController.Move(moveDirection * Time.deltaTime);
+        charController.Move(moveDirection * speed * Time.deltaTime);
     }
 
     public void Update() 
         {
-       
             MoveChar();
-            
-           
-            //transform.Rotate(rotateDirection);
-
-            //moveDirection = transform.TransformDirection(moveDirection);
         }
         
 
         /*if (moveDirection != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation( moveDirection),0.15f );
-        }*/
+        }
+        void Rotate()
+        {
+        transform.Rotate(0, 90, 0);
+        }
+    
+        //transform.Rotate(rotateDirection);
+
+        //moveDirection = transform.TransformDirection(moveDirection);*/
         
     }
 
-    /*void Rotate()
-    {
-        transform.Rotate(0, 90, 0);
-    }*/
+    
 
     
 
