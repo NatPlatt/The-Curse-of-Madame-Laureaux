6ca1cf71b7e32c4042b27ad1b6a4cec84fd595a5
NatPlatt <n.zollinger26@hotmail.com>
2020-03-25 18:21:40 -0600

    writing bool events script

--- Assets/Scripts/BoolEvents.cs
+++ Assets/Scripts/BoolEvents.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BoolEvents : MonoBehaviour
 {
-    
-    
+    public bool boolCondition = false;
+
+    public UnityEvent trueEvent;
+    public UnityEvent falseEvent;
+
+    public void RunTheEvent()
+    {
+        if (boolCondition == true)
+        {
+            trueEvent.Invoke();
+        }
+        else
+        {
+            falseEvent.Invoke();
+        }
+    }
+
 }
