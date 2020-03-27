fb9edd29e51a0ce3b94170d05e3ee032baa3d234
NatPlatt <n.zollinger26@hotmail.com>
2020-03-27 13:54:19 -0600

    trying to get hidden objects to work

--- Assets/Scripts/HiddenObjects.cs
+++ Assets/Scripts/HiddenObjects.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 
 public class HiddenObjects : Interactable
 {
+    public GameObject hiddenObj; 
     public override void Interact()
     {
         base.Interact();
         Debug.Log("You have found hidden clue 1");
+        
+    }
+
+    public override void OnMouseDown()
+    {
+        base.OnMouseDown();
+        gameObject.GetComponent<Renderer>().material.color = clickedColor;
     }
     
 }
