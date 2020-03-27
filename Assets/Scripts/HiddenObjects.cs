2b6da6c9a0e939da45eb03415136a30de412fbc7
NatPlatt <n.zollinger26@hotmail.com>
2020-03-27 11:34:20 -0600

    scripting for hidden objects

--- Assets/Scripts/HiddenObjects.cs
+++ Assets/Scripts/HiddenObjects.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class HiddenObjects : Interactable
 {
+    public Transform playerTransform;
     public GameObject player;
+    public float hiddenObjBounds = 4;
+    public float distance;
+   
+    protected void Update()
+    {
+        float distance = Vector3.Distance(playerTransform.position, transform.position);
+    }
     
     public override void Interact()
     {
-        if (player.transform <= )
-        base.Interact();
-        Debug.Log("You have found hidden clue 1");
+        if (distance <= hiddenObjBounds)
+        {
+             base.Interact();
+             Debug.Log("You have found hidden clue 1");
+        }
+        
     }
 }
