88272c83ce236af41ba0dc9fbdf9a4cffa64bb86
NatPlatt <n.zollinger26@hotmail.com>
2020-03-27 11:20:46 -0600

    Update HiddenObjects.cs

--- Assets/Scripts/HiddenObjects.cs
+++ Assets/Scripts/HiddenObjects.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class HiddenObjects : Interactable
 {
+    public GameObject player;
+    
     public override void Interact()
     {
+        if (player.transform <= )
         base.Interact();
         Debug.Log("You have found hidden clue 1");
     }
