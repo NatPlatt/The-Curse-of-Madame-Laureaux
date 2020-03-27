2ba768bebfe3695020ed9ebc53c8cc7adb3d6ad3
NatPlatt <n.zollinger26@hotmail.com>
2020-03-27 11:36:50 -0600

    Update HiddenObjects.cs

--- Assets/Scripts/HiddenObjects.cs
+++ Assets/Scripts/HiddenObjects.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class HiddenObjects : Interactable
 {
-    public Transform playerTransform;
     public GameObject player;
     public float hiddenObjBounds = 4;
-    public float distance;
-   
-    protected void Update()
+    public float playerDistance;
+
+    void Update()
     {
-        float distance = Vector3.Distance(playerTransform.position, transform.position);
+        playerDistance = player.transform.position.x;
     }
     
     public override void Interact()
     {
-        if (distance <= hiddenObjBounds)
+        if ( playerDistance <= hiddenObjBounds)
         {
              base.Interact();
              Debug.Log("You have found hidden clue 1");
