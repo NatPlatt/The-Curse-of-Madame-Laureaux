51157eddcdca93a3acde5188a3389ae1ddd4f101
NatPlatt <n.zollinger26@hotmail.com>
2020-03-29 15:39:51 -0600

    trying to disable components

--- Assets/Scripts/DisableObjs.cs
+++ Assets/Scripts/DisableObjs.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class DisableObjs : ScriptableObject
 {
-    public Canvas disableThisCanvas;
+    public Text text;
+    public Button button;
     
     public void DisableIt()
     {
-        disableThisCanvas.enabled = false;
+        text.enabled = false;
+        button.enabled = false;
     }
 }
