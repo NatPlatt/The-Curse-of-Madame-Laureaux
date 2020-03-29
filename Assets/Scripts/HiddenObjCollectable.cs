221b4f8f5eb8bd7f1d3faf9e4e3c3410fd890092
NatPlatt <n.zollinger26@hotmail.com>
2020-03-29 15:07:53 -0600

    writing collectable script
    for hidden objects

---
+++ Assets/Scripts/HiddenObjCollectable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HiddenObjCollectable : Collectable
+{
+    public UnityEvent useEvent;
+
+    public override void Use()
+    {
+        
+    }
+}
