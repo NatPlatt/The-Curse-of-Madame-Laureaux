be54d61381c59b6539ba2ed3faaa3f07bd7596df
NatPlatt <n.zollinger26@hotmail.com>
2020-03-29 20:15:57 -0600

    fixed skinny path triggers

--- Assets/Scripts/TriggerEvents.cs
+++ Assets/Scripts/TriggerEvents.cs
@@ -1,17 +1,22 @@
 ﻿
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEnterEvent;
-
+    public UnityEvent triggerExitEvent;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "enemy")
         {
            triggerEnterEvent.Invoke(); 
-        } 
-        
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
     }
 }
