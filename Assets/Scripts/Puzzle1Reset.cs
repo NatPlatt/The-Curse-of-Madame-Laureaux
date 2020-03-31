2a0b18d435082020d23cd6fc6efb0fdabc9b723e
NatPlatt <n.zollinger26@hotmail.com>
2020-03-30 18:40:40 -0600

    writing script for respawn

---
+++ Assets/Scripts/Puzzle1Reset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1Reset : MonoBehaviour
+{
+    public GameObject playerObj;
+    public float minHeightForReset;
+    public Transform respawnPosition;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (playerObj.transform.position.y < minHeightForReset)
+        {
+            playerObj.transform.position = respawnPosition.position;
+        }
+    }
+}
