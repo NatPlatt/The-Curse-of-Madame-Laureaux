75346e13ad9c9876199b7cbfb895deebaeb4c916
NatPlatt <n.zollinger26@hotmail.com>
2020-04-02 20:49:10 -0600

    trying to instantiate coins

---
+++ Assets/Scripts/InstantiateOnEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InstantiateOnEnemy : ScriptableObject
+{
+    public GameObject coin;
+    public Transform transform;
+
+    public void GetYourCoins()
+    {
+        Instantiate(coin, transform.position, Quaternion.identity);
+    }
+}
