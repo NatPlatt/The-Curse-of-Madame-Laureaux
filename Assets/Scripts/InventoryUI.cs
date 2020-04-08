f778d9979a26d0df2a0b5505c48337af689dbcd9
NatPlatt <n.zollinger26@hotmail.com>
2020-04-07 19:24:06 -0600

    adding to inventory script

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class InventoryUI : MonoBehaviour
 {
     public List<GameObject> inventorySlots;
+    public List<GameObject> inventoryButtons;
 
     public GameObject hiddenObjectImage, coinImage;
 
@@ -14,11 +15,46 @@ public class InventoryUI : MonoBehaviour
 
     public PickUpObj.pickUpType pickUpType;
 
+    public GameObject hiddenObjButtonPrefab;
+    public GameObject inventoryUI;
+
     public void InventoryManager(GameObject pickUp)
     {
         //what pickup object is it
         pickUpType = pickUp.GetComponent<PickUpObj>.pickUps;
         //turn on that image and value in the inventory 
         AddToInventory(pickUpType);
+    }
+
+    public void AddToInventory(PickUpObj.pickUpType type)
+    {
+        if (type == PickUpObj.pickUpType.COINS)
+        {
+            coinCount += 10;
+            coinUI.text = coinCount.ToString();
+            return;
+        }
+        
+        //find an empty slot in the inventory
+        int index = 0;
+        for (int i = 0; i < inventoryButtons.Count; i++)
+        {
+            if (inventoryButtons[i] == null)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        GameObject buttonToAdd = null;
+        if (type == PickUpObj.pickUpType.HIDDENOBJS)
+        {
+            buttonToAdd = hiddenObjButtonPrefab;
+            //instantiate prefab and put it in the inventory
+            inventoryButtons[index] =
+                Instantiate(buttonToAdd, inventorySlots[index].transform.position, Quaternion.identity);
+            //make prefab appear in the inventory menu
+            inventoryButtons[index].transform.SetParent(inventoryUI.transform);
+        }
     }
 }
