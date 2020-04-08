5d1ca8937a2f363ee621c639cd2d475efcea93fd
NatPlatt <n.zollinger26@hotmail.com>
2020-04-07 18:10:53 -0600

    writing inventory scripts

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    public List<GameObject> inventorySlots;
+
+    public GameObject hiddenObjectImage, coinImage;
+
+    public int coinCount = 0;
+    public Text coinUI;
+    
+    
+}
