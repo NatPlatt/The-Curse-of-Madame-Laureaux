00ceee55e62d394a2aa23fa800ebd59e235c9201
NatPlatt <n.zollinger26@hotmail.com>
2020-04-07 18:17:14 -0600

    writing script for inventory objects

--- Assets/Scripts/PickUpObj.cs
+++ Assets/Scripts/PickUpObj.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class PickUpObj : MonoBehaviour
 {
-    public GameObject pickUpObj;
+
+    public GameObject inventory;
+
+    public enum pickUpType
+    {
+        COINS, HIDDENOBJS
+    }
+
+    /*public GameObject pickUpObj;
     public GameObject holdObj;
 
     public void PickUpSomething()
     {
         pickUpObj.transform.position = holdObj.transform.position;
         pickUpObj.transform.SetParent(holdObj.transform);
-    }
+    }*/
 }
