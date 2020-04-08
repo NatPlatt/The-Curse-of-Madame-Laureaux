27c036620e79c89cb7baad66a9367b169060003e
NatPlatt <n.zollinger26@hotmail.com>
2020-04-08 13:17:55 -0600

    trying to get coin value to update correctly

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -31,6 +31,7 @@ public class InventoryUI : MonoBehaviour
     {
         if (type == PickUpObj.pickUpType.COINS)
         {
+            coinAmount.UpdateValue(coinCount);
             coinCount = coinAmount.value;
             coinUI.text = coinCount.ToString() + "$";
             Debug.Log("You get" + coinCount + "coins");
