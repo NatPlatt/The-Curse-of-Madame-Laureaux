a1003d762006dab37fa127e8cad3cf19076615d0
NatPlatt <n.zollinger26@hotmail.com>
2020-04-09 21:45:14 -0600

    working on shop

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public Collection HOcollection;
+    public IntData coinAmount;
+    public IntData HOAmount;
+    public GameObject buyButton;
+    public GameObject allGoneText;
+    public GameObject HOsprite;
+    public Transform[] storePoints;
+    
+    void BuyObjects()
+    {
+        if (HOcollection.collectablesList.Count == HOAmount.value)
+        {
+            buyButton.SetActive(false);
+            allGoneText.SetActive(true);
+            return;
+        }
+        if (HOcollection.collectablesList.Count < HOAmount.value)
+        {
+            Instantiate(HOsprite, storePoints, Quaternion.identity);
+        }
+    }
+}
