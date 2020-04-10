f390987309db06b56e721bc1f3e71c42efb5aa79
NatPlatt <n.zollinger26@hotmail.com>
2020-04-09 18:44:52 -0600

    working on inventory

---
+++ Assets/Scripts/UI/ClickItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickItem : MonoBehaviour
+{
+    public GameObject canvas;
+
+    public void OnClick()
+    {
+        canvas = GameObject.Find("Canvas");
+        //canvas.GetComponent<QuickSelectMenu>().quickSelectTextBox.SetActive(true);
+    }
+}
