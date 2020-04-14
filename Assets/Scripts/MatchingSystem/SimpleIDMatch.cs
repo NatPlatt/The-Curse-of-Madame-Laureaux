48509be13108da1b9b792adc762ca180916e189e
NatPlatt <n.zollinger26@hotmail.com>
2020-04-13 18:52:58 -0600

    added anthony script
    for simple match id behaviors, what happens when there isn't a match

---
+++ Assets/Scripts/MatchingSystem/SimpleIDMatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleIDMatch : IdBehavior
+{
+    protected NameId OtherIdObj;
+    public UnityEvent matchEvent, noMatchEvent;
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        OtherIdObj = other.GetComponent<NameId>();
+
+        if (nameIdObj == OtherIdObj)
+        {
+            matchEvent.Invoke();
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+        }
+    }
+}
+
+
