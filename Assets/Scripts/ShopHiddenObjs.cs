6b5a1df9ca30be5a0d9c2a74a971db60b54bc324
NatPlatt <n.zollinger26@hotmail.com>
2020-04-23 08:03:03 -0600

    Update ShopHiddenObjs.cs

--- Assets/Scripts/ShopHiddenObjs.cs
+++ Assets/Scripts/ShopHiddenObjs.cs
@@ -8,18 +8,13 @@ public class ShopHiddenObjs : MonoBehaviour
     public Collection HOcollection;
     public Collectable HObject;
     public Button hiddenObjButton;
-    public List<Collectable> hiddenObjSOs;
-    public GameObject[] buttons;
+    
     
     public void IsActive()
     {
-        if (HOcollection.collectablesList.Count <= hiddenObjSOs.Count)
+        if (HOcollection.collectablesList.Contains(HObject))
         {
-            for (int i = 0; i < HOcollection.collectablesList.Count; i ++ )
-            {
-                gameObject.SetActive(hiddenObjButton);
-                Debug.Log("the buttons should be active");
-            }
+            gameObject.SetActive(hiddenObjButton);
         }
     }
 }
