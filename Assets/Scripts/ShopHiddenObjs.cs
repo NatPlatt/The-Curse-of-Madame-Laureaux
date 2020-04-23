4434df5f8dc0350640900353275122d1fb022551
NatPlatt <n.zollinger26@hotmail.com>
2020-04-22 23:52:27 -0600

    adding scripts for shop

---
+++ Assets/Scripts/ShopHiddenObjs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopHiddenObjs : MonoBehaviour
+{
+    public Collection HOcollection;
+    public Collectable HObject;
+    public Button hiddenObjButton;
+    public GameObject[] hiddenObjSOs;
+    public GameObject[] buttons;
+    
+    public void IsActive()
+    {
+        if (HOcollection.collectablesList.Count == hiddenObjSOs.Length)
+        {
+            foreach (var gameObject in buttons)
+            {
+                gameObject.SetActive(hiddenObjButton);
+            }
+        }
+    }
+}
