82eb81b501849956201c082291f95dc1da7a5bf1
NatPlatt <n.zollinger26@hotmail.com>
2020-04-23 19:27:45 -0600

    writing script for spawn player
    got help from Jessica with this

---
+++ Assets/Scripts/ResetPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour
+{
+    public GameObject respawnPoint;
+
+    public void OnReset()
+    {
+        GetComponent<CharacterController>().enabled = false;
+    }
+}
