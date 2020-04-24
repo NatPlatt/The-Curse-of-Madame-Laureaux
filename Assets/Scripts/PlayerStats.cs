21f4c7db134bd0b9e10f1bce711a26cb987ce9f4
NatPlatt <n.zollinger26@hotmail.com>
2020-04-24 14:14:37 -0600

    Update PlayerStats.cs

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,21 +5,23 @@ using UnityEngine.SceneManagement;
 public class PlayerStats : CharStats
 {
     public Color deadColor = Color.black;
-    //public GameObject killScreen;
+    
     public HeartData heartData;
 
     public override void Die()
     {
         base.Die();
-        //killScreen.SetActive(true);
-        heartData.currentHearts--;
+        
+        heartData.onLoseHeartEvent.Invoke();
         
         gameObject.GetComponent<Renderer>().material.color = deadColor;
-        if (heartData.currentHearts <= 0)
-        {
-            SceneManager.LoadScene("KillScreen");
-        }
+        
         PlayerManager.instance.KillPlayer();
+    }
+
+    public void DieForReal()
+    {
+        SceneManager.LoadScene("KillScreen");
     }
 
 
