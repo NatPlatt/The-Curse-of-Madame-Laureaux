35238638adb4f19ec6b9d011d187569289f297b6
NatPlatt <n.zollinger26@hotmail.com>
2020-04-24 13:35:52 -0600

    Update HeartsUI.cs

--- Assets/Scripts/HeartsUI.cs
+++ Assets/Scripts/HeartsUI.cs
@@ -4,5 +4,63 @@ using UnityEngine;
 
 public class HeartsUI : MonoBehaviour
 {
-    
+    public GameObject heartOne, heartTwo, heartThree, heartFour, heartFive;
+    public HeartData heartData;
+
+    public void DisplayHearts()
+    {
+        if (heartData.currentHearts == 5)
+        {
+            heartOne.SetActive(true);
+            heartTwo.SetActive(true);
+            heartThree.SetActive(true);
+            heartFour.SetActive(true);
+            heartFive.SetActive(true);
+        }
+        
+        if (heartData.currentHearts == 4)
+        {
+            heartOne.SetActive(true);
+            heartTwo.SetActive(true);
+            heartThree.SetActive(true);
+            heartFour.SetActive(true);
+            heartFive.SetActive(false);
+        }
+        
+        if (heartData.currentHearts == 3)
+        {
+            heartOne.SetActive(true);
+            heartTwo.SetActive(true);
+            heartThree.SetActive(true);
+            heartFour.SetActive(false);
+            heartFive.SetActive(false);
+        }
+        
+        if (heartData.currentHearts == 2)
+        {
+            heartOne.SetActive(true);
+            heartTwo.SetActive(true);
+            heartThree.SetActive(false);
+            heartFour.SetActive(false);
+            heartFive.SetActive(false);
+        }
+        
+        if (heartData.currentHearts == 1)
+        {
+            heartOne.SetActive(true);
+            heartTwo.SetActive(false);
+            heartThree.SetActive(false);
+            heartFour.SetActive(false);
+            heartFive.SetActive(false);
+        }
+        
+        if (heartData.currentHearts == 0)
+        {
+            heartOne.SetActive(false);
+            heartTwo.SetActive(false);
+            heartThree.SetActive(false);
+            heartFour.SetActive(false);
+            heartFive.SetActive(false);
+        }
+    }
 }
