82214b52c192c8de2ba2bb8b460ec6a711979628
NatPlatt <n.zollinger26@hotmail.com>
2020-04-24 12:12:36 -0600

    trying to get kill screen to work

---
+++ Assets/Scripts/KillScreen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillScreen : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+
+    public GameObject killScreenUI;
+
+    private void Start()
+    {
+        GameIsPaused = false;
+        Resume();
+    }
+
+    void Update()
+    {
+        if (killScreenUI.activeInHierarchy)
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+    
+    public void Resume()
+    {
+        killScreenUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        killScreenUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
