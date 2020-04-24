643ce65e7c05929fd14b73a58a4323fe6ea48164
NatPlatt <n.zollinger26@hotmail.com>
2020-04-24 13:53:17 -0600

    adding hearts to UI

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,13 +6,18 @@ public class PlayerStats : CharStats
 {
     public Color deadColor = Color.black;
     //public GameObject killScreen;
+    public HeartData heartData;
 
     public override void Die()
     {
         base.Die();
         //killScreen.SetActive(true);
-        //SceneManager.LoadScene("KillScreen");
+        
         gameObject.GetComponent<Renderer>().material.color = deadColor;
+        if (heartData.currentHearts <= 0)
+        {
+            SceneManager.LoadScene("KillScreen");
+        }
         PlayerManager.instance.KillPlayer();
     }
 
