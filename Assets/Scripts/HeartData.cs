7a8f182f3aac29051d163218937b19a43733902a
NatPlatt <n.zollinger26@hotmail.com>
2020-04-24 13:29:08 -0600

    writing heart scripts

---
+++ Assets/Scripts/HeartData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HeartData : MonoBehaviour
+{
+    public int currentHearts;
+
+    public UnityEvent deathEvent, onLoseHeartEvent;
+
+    private void Update()
+    {
+        if (currentHearts <= 0)
+        {
+            deathEvent.Invoke();
+        }
+    }
+
+    public void LoseHeart()
+    {
+        currentHearts--;
+        onLoseHeartEvent.Invoke();
+    }
+}
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 public class PlayerStats : CharStats
 {
     public Color deadColor = Color.black;
-    public GameObject killScreen;
+    //public GameObject killScreen;
 
     public override void Die()
     {
         base.Die();
-        killScreen.SetActive(true);
+        //killScreen.SetActive(true);
         //SceneManager.LoadScene("KillScreen");
         gameObject.GetComponent<Renderer>().material.color = deadColor;
         PlayerManager.instance.KillPlayer();
