598531d54a362f843e9888f2190708f808da8495
NatPlatt <n.zollinger26@hotmail.com>
2020-04-24 23:21:38 -0600

    added music

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public GameAction Action;
+    private AudioSource audioSource;
+    void Start()
+    {
+        Action.action += StopMusic;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
